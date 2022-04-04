2facee2d5ec37db019052a5499eb34cfc4f314c0
hellomeerim <eg.meerim@gmail.com>
2022-04-04 21:17:32 +0600

    fix

--- src/pages/Article/Comments.jsx
+++ src/pages/Article/Comments.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import styles from "./Comments.module.css"
-import { comments } from '../../constant';
+import { BASE_URL, comments } from '../../constant';
 
 const Comments = () => {
     const [name, setName] = useState('');
@@ -13,6 +13,28 @@ const Comments = () => {
     const getComment =(event) => {
         setComment(event.currentTarget.value);
     }
+
+    const addUsers = () => {
+        const url = BASE_URL + "/users";
+    
+        const user = {
+          name: setName,
+          comment: setComment
+        }
+    
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(user)
+        }
+    
+        fetch(url,options)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+      }
+
     return (
         <div className={styles.comments}>
             <h4>Комментарии</h4>
@@ -20,7 +42,7 @@ const Comments = () => {
                 <img src="https://sevelina.ru/images/uploads/2014/02/287.png" alt="" />
                 <input onChange={getName} className={styles.container1} type="text" placeholder='Введите имя'/>
                 <input onChange={getComment}  type="text" placeholder='Введите текст комментария'/>
-                <button>Оставить комментарий</button> 
+                <button onClick={addUsers}>Оставить комментарий</button> 
             </div><br /> <br />
             <h5>122 комментариев {
                  comments.map((item) => {
